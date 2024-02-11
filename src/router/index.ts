ba4003e5b055defd25bb6e45d2ab09959c4b70d9
Vital Raicheu <rvitaly1978@gmail.com>
2024-02-11 04:33:47 +0300

    feat: implement router

---
+++ src/router/index.ts
@@ -0,0 +1,36 @@
+import { IncomingMessage } from 'http'
+import { HttpResponse, HttpMethod } from '../types'
+import { getUsers, createUser, getUser, updateUser, deleteUser } from '../controllers'
+import {
+  notFoundResourceResponse, badRequestResponse, methodNotAllowedResponse,
+  matchBaseUrl, matchValidUserIdUrl, matchInvalidUserIdUrl,
+} from '../helpers'
+
+export const router = async (req: IncomingMessage ): Promise<HttpResponse> => {
+  const { method, url } = req
+
+  console.log (`[CRUD-API] Request ${method}: ${url}`)
+
+  if (matchBaseUrl(url)) {
+    switch (method) {
+      case HttpMethod.Get: return getUsers()
+      case HttpMethod.Post: return (await createUser(req))
+      default: return methodNotAllowedResponse
+    }
+  }
+
+  if (matchValidUserIdUrl(url)) {
+    switch (method) {
+      case HttpMethod.Get: return getUser(req)
+      case HttpMethod.Put: return (await updateUser(req))
+      case HttpMethod.Delete: return deleteUser(req)
+      default: return methodNotAllowedResponse
+    }
+  }
+
+  if (matchInvalidUserIdUrl(url)) {
+    return badRequestResponse
+  }
+
+  return notFoundResourceResponse
+}
